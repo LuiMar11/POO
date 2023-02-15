9b0bce711fdc6b874e6b78b3d1e59b5dfec1d8e4
LuiMar11 <64877820+LuiMar11@users.noreply.github.com>
2023-02-15 16:23:39 -0500

    Add abstraccion

---
+++ abstraccion.js
@@ -0,0 +1,9 @@
+class Animal {
+  constructor(nombre,especie) {
+          this.nombre = nombre;
+          this.especie = especie;
+  }
+}
+
+let dog = new Animal('Perro','Mamífero');
+console.log(dog);
